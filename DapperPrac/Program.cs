3a6b250f00d7b1df18b34c1c354d760b345283e9
Harold Bengter <Haroldbe55@yahoo.com>
2022-08-01 11:57:34 -0400

    MAde copy to newer in json

--- DapperPrac/Program.cs
+++ DapperPrac/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿using System;
+using System.Data;
+using System.IO;
+using MySql.Data.MySqlClient;
+using Microsoft.Extensions.Configuration;
+//^^^^MUST HAVE USING DIRECTIVES^^^^
+
+var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+string connString = config.GetConnectionString("DefaultConnection");
+IDbConnection conn = new MySqlConnection(connString);
